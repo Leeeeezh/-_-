1012548fd9989dbb87784ea54daeecf4e5fadbc2
Zihua Li <lzh1757657118@outlook.com>
2019-09-03 16:29:47 +0800

    Initial Commit

---
+++ components/like/index.js
@@ -0,0 +1,40 @@
+Component({
+  properties: {
+    count: {
+      type: Number,
+      value: 0,
+      observer: function () {
+
+      }
+    },
+    isLike: {
+      type: Boolean,
+      value: false,
+      observer: function () {
+
+      }
+    }
+  },
+  data: {
+    likeImg: 'images/like.png',
+    dislikeImg: 'images/dislike.png'
+  },
+  methods: {
+    onLikeTap(event) {
+      let count = this.properties.count
+      let isLike = this.properties.isLike
+      console.log(count, isLike)
+      if (!isLike) {
+        this.setData({
+          count: count + 1,
+          isLike: true
+        })
+      } else {
+        this.setData({
+          count: count - 1,
+          isLike: false
+        })
+      }
+    }
+  }
+})
